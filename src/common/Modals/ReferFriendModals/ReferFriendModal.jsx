21c0797aeaa678683c99d924ec3595cdaaa0e9c2
Akay <akaudeh@gmail.com>
2022-12-13 13:26:42 +0100

    Fixed ReferFriendModal.jsx

--- src/common/Modals/ReferFriendModals/ReferFriendModal.jsx
+++ src/common/Modals/ReferFriendModals/ReferFriendModal.jsx
@@ -1,30 +1,44 @@
 import './ReferFriendModal.css';
 import { useState } from "react";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
 function ReferFriendModal(props) {
   const { setReferFriendModal } = props;
   const [referralAddress, setReferralAddress] = useState("");
 
   return (
-    <div className="referFriendModal">
-      <div className="referFriendModalTitle">Refer & Earn</div>
-      <div className="referFriendModalContent">
-        <div>
-          <div>
-            <input
-                className="modalInput"
-                type="text"
-                value={referralAddress}
-                placeholder="NFT ID"
-                onChange={ async(e) => {
-                  setReferralAddress(e.target.value);
-                }}
-              />
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="closeModalBtn">
+          <CancelOutlinedIcon
+            onClick={() => setReferFriendModal(false)}
+            className="button"
+          />
+        </div>
+        <div className="modalContent">
+          <div className="referFriendModal">
+            <div className="referFriendModalTitle">Refer & Earn</div>
+            <div className="referFriendModalContent">
+              <div>
+                <div>
+                  <input
+                    className="modalInput"
+                    type="text"
+                    value={referralAddress}
+                    placeholder="NFT ID"
+                    onChange={ async(e) => {
+                      setReferralAddress(e.target.value);
+                    }}
+                  />
+                </div>
+                <button>Refer Friend</button>
+              </div>
+            </div>
           </div>
-          <button>Refer Friend</button>
         </div>
       </div>
     </div>
+
   );
 }
 
